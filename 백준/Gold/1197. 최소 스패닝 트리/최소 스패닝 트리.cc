d79098e4cfc971280df5a79b264b7e298e440378
Yewon Jeong <101704967+ye-one222@users.noreply.github.com>
2024-06-25 20:46:06 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 140 ms, Memory: 7320 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,46 +1,44 @@
+//Prim's Algo. 이용
 #include <iostream>
-#include <tuple>
 #include <vector>
+#include <queue>
+#include <tuple>
 #include <algorithm>
 
 using namespace std;
 
-vector<int> p(10005, -1);
-tuple<int, int, int> edge[100005];
-
-int find(int x) {
-	if (p[x] < 0) return x; //자기 자신이 루트 노드임
-	return p[x] = find(p[x]); //최종 루트를 찾음 (경로 압축을 위해 p[x]에 직접 저장)
-}
-
-bool is_diff_group(int a, int b) {
-	a = find(a); b = find(b); //a와 b에 각각의 루트 노드 저장
-	if (a == b) return 0; //루트 노드 같음 => 같은 그룹
-	//아래는 루트가 다를 경우 집합 합치는 거
-	if (p[a] == p[b])p[a]--; //같은 크기 집합일 때
-	if (p[a] < p[b])p[b] = a; //큰 집합이 작은 집합을 흡수하도록
-	else p[a] = b;
-	return 1;
-}
-
+vector<pair<int, int>> adj[10005]; //cost, 정점번호
+bool check[10005]; //해당 정점이 MST에 속해있는지
 int main() {
 	int V, E, a, b, cost;
+	int cnt = 0; int ans = 0;
 	cin >> V >> E;
 
 	for (int i = 0; i < E; i++) {
 		cin >> a >> b >> cost;
-		edge[i] = { cost,a,b };
+		adj[a].push_back({ cost,b });
+		adj[b].push_back({ cost,a });
 	}
-	sort(edge, edge + E);
-
-	int cnt = 0;
-	int ans = 0;
-	for (int i = 0; i < E; i++) {
-		tie(cost, a, b) = edge[i]; //cost, a, b에 각각 할당됨
-		if (!is_diff_group(a, b)) continue;
+	
+	priority_queue<tuple<int, int, int>, //cost, 정점1, 정점2
+		vector<tuple<int, int, int>>,
+		greater<tuple<int, int, int>>> pq; //최소 힙
+	
+	check[1] = 1; //처음 시작점을 1로
+	for (auto next : adj[1]) {
+		pq.push({ next.first,1,next.second });
+	}
+	while (cnt < V - 1) {
+		tie(cost, a, b) = pq.top(); //최소 비용의 간선
+		pq.pop();
+		if (check[b])continue;
 		ans += cost;
+		check[b] = true;
 		cnt++;
-		if (cnt == V - 1) break;
+		for (auto next : adj[b]) {
+			if (!check[next.second]) //MST에 없는 정점이라면
+				pq.push({ next.first,b,next.second });
+		}
 	}
 	cout << ans;
 }
