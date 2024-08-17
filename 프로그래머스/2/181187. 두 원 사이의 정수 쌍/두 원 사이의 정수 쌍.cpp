1f17e30250b99ed80c0880c891a83a9305b7fdf1
Yewon Jeong <101704967+ye-one222@users.noreply.github.com>
2024-08-17 19:33:17 +0900

    Update 두 원 사이의 정수 쌍.cpp
    변수 선언 위치 수정

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cpp
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cpp
@@ -4,13 +4,13 @@
 
 using namespace std;
 
-int a,b;
 long long solution(int r1, int r2) {
     long long answer = 0;
+    int minY =0; int maxY=0;
     
     for(int i=1; i<=r2; i++){
-        int minY = (int)ceil(sqrt((long long)r1*r1 - (long long)i*i));
-        int maxY = (int)floor(sqrt((long long)r2*r2 - (long long)i*i));
+        minY = ceil(sqrt((long long)r1*r1 - (long long)i*i));
+        maxY = floor(sqrt((long long)r2*r2 - (long long)i*i));
         
         if(i>r1)
             minY = 0;
