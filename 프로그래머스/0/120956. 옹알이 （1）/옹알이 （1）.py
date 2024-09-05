287c2b4bc9183f8b6a1e392450c171f2f99dec96
Yewon Jeong <101704967+ye-one222@users.noreply.github.com>
2024-09-06 01:13:07 +0900

    [level 0] Title: 옹알이 (1), Time: 0.11 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,23 +1,13 @@
+pos = ["aya", "ye", "woo", "ma"]
+
 def solution(babbling):
     answer = 0
-    bab_length = len(babbling)
     
-    for i in range(bab_length) :
-        flag = 1
-        j = 0
-        while j < len(babbling[i]) :
-            if babbling[i][j:j+3]=="aya" :
-                j += 3
-            elif babbling[i][j:j+2]=="ye" :
-                j += 2
-            elif babbling[i][j:j+3]=="woo" :
-                j += 3
-            elif babbling[i][j:j+2]=="ma" :
-                j += 2
-            else :
-                flag = 0
-                break
-
-        if flag==1 :
+    for i in babbling :
+        for j in pos :
+            i = i.replace(j," ")
+        i = i.replace(" ","")
+        if len(i)==0 :
             answer += 1
+    
     return answer
